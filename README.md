511a6e459194348cd80bec8580cb78c0a3479c58
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2020-10-29 22:21:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Webpack Template Layout for React with Redux with Redux-Thunk library, and with SASS (syntax SCSS).
+
+> This is my base config for React with Webpack module bundler with Redux library and Redux-Thunk middleware.
+
+## Quick Start
+
+# Step 1 Clone repo
+
+```bash
+git clone git@github.com:konstantindergachev/webpack-template-react-with-redux-and-sass.git
+```
+
+# Step 2 Install dependencies
+
+```bash
+yarn -i || npm install
+```
+
+# Step 3 Use your own social icons
+
+# Step 4 Use your own favicon.ico and favicon directory in the src/img
+
+# Step 5 Start this react template on localhost:3000
+
+```bash
+yarn dev || npm run dev
+```
+
+# Step 6 Buid your project for production
+
+```bash
+yarn build || npm run build
+```
+
+## Info
+
+### Author
+
+Konstantin Dergachev [portfolio](http://dergachevkonstantin.surge.sh/).
+
+### License
+
+This project is licensed under the MIT License
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+const cssProd = [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'];
+const cssConfig = isProd ? cssProd : cssDev;
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: cssConfig,
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/,
+        use: [{ loader: 'file-loader?name=img/[name].[ext]' }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: !isProd ? '[name].css' : '/css/[name].[hash].css',
+      chunkFilename: !isProd ? '[id].css' : '/css/[id].[hash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/img/favicon', to: 'img/favicon', toType: 'dir' },
+        { from: 'src/img/favicon.ico', to: 'favicon.ico', toType: 'file' },
+      ],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
+  devServer: {
+    noInfo: true,
+    overlay: true,
+    historyApiFallback: true, //to 404s-error will fallback to index.html
+    open: true,
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+};
+
+module.exports = (env, options) => {
+  const production = options.mode === 'production';
+  config.devtool = production ? false : 'eval-cheap-module-source-map'; // best source-map
+  return config;
+};
